7ab76c004cd7f11ade15eb62d3f61b07718a2d01
ChristianHardin <christianhardin@proton.me>
2024-09-03 19:14:32 -0400

    feat: added bogus board

---
+++ keyboards/handwired/herman/bogus/keymaps/default/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define SPLIT_USB_DETECT
+
+// Vial Support
+#define VIAL_KEYBOARD_UID {0x32, 0xFC, 0x8F, 0x16, 0x47, 0xF8, 0x4B, 0x8E}
+
+// The four corners
+#define VIAL_UNLOCK_COMBO_ROWS {0,0}
+#define VIAL_UNLOCK_COMBO_COLS {0,1}
